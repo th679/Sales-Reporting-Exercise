18420bbb1f07908cb4de0fc024fa8ac950660a20
th679 <th679@georgetown.edu>
2019-02-06 15:40:28 -0500

    Calculate total sales

--- sales-reporting.py
+++ sales-reporting.py
@@ -2,8 +2,20 @@ import csv
 
 csv_file_path = "data.csv"
 
+product_sales =[]
+
 with open(csv_file_path, "r") as csv_file:
     reader = csv.DictReader(csv_file)
-
+    for row in reader:
+        d = {"product": row["product"], "unit price": row["unit price"], "units sold": row["units sold"], "sales price": float(row["sales price"])}
+        product_sales.append(d)
 
 print("SALES REPORT (MONTH YEAR)")
+
+total_sales = 0
+
+for p in product_sales:
+    total_sales = total_sales + p["sales price"]
+total_sales_usd = "${0:.2f}".format(total_sales)
+
+print(total_sales_usd)
