afb7111f3cc46879389c75ea8ccc9e2acd17ea8f
th679 <th679@georgetown.edu>
2019-02-06 15:55:25 -0500

    Format total sales

--- sales-reporting.py
+++ sales-reporting.py
@@ -7,7 +7,7 @@ product_sales =[]
 with open(csv_file_path, "r") as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
-        d = {"product": row["product"], "unit price": row["unit price"], "units sold": row["units sold"], "sales price": float(row["sales price"])}
+        d = {"date": ["date"], "product": row["product"], "unit price": row["unit price"], "units sold": row["units sold"], "sales price": float(row["sales price"])}
         product_sales.append(d)
 
 print("SALES REPORT (MONTH YEAR)")
@@ -16,6 +16,7 @@ total_sales = 0
 
 for p in product_sales:
     total_sales = total_sales + p["sales price"]
-total_sales_usd = "${0:.2f}".format(total_sales)
 
-print(total_sales_usd)
+total_sales_usd = "${0:,.2f}".format(total_sales,)
+
+print("TOTAL SALES: " + total_sales_usd)
